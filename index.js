310962364f4d49a600af6cc8137e5e1aa1c0f549
blest78 <blest78@outlook.com>
2025-01-31 14:31:31 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import http from 'http';
+import axios from 'axios';
+
+const targetUrl = 'https://hiveonboard.com/api/tickets';
+
+const server = http.createServer((req, res) => {
+  let body = '';
+
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    const { accessToken } = JSON.parse(body);
+
+    const requests = Array.from({ length: 20 }).map(() => 
+      axios.post(targetUrl, { accessToken })
+    );
+
+    Promise.all(requests)
+      .catch(() => {});
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Requests sent to target API.');
+  });
+});
+
+server.listen(3000);
